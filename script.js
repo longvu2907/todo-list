c670aee087d074b8438d400a28a54334fe3cd405
Vu Nguyen <74065518+longvu2907@users.noreply.github.com>
2021-07-29 22:51:17 +0700

    api url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const todoListApi = "http://localhost:3000/todoList";
+const todoListApi = "https://raw.githubusercontent.com/longvu2907/todo-list/main/db.json";
 const todoListBlock = document.querySelector('.todo-list__list');
 const addButton = document.querySelector('.add-todo__button');
 var addButtonActive = document.querySelector('.add-todo__button.active');
